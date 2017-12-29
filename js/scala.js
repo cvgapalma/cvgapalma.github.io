01d1e987513ea143feccdda25a062b609e3c2e8b
Erick Palma <ep434@cornell.edu>
2017-12-29 01:59:21 -0600

    Added cumulative totals, and icons as text for formatting issue fixes on mobile phones.

--- js/scala.js
+++ js/scala.js
@@ -1,42 +1,78 @@
+function isMobile() {
+  return /Mobi/.test(navigator.userAgent);
+}
+
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
 function bco() {
   $("#iTpO").prop("checked", true);
   $("#iTpO").prop("checked", true).trigger("click");
   $("#pesoSca").val("130");
-  var selec = document.getElementById("pesoAni");
-  selec.focus();
-  selec.setSelectionRange(0, selec.value.length);
+  if (!isMobile()) {
+    var selec = document.getElementById("pesoAni");
+    selec.focus();
+    selec.setSelectionRange(0, selec.value.length);
+  }
+  return false;
 }
 
 function bca() {
   $("#iOpO").prop("checked", true);
   $("#iOpO").prop("checked", true).trigger("click");
-  $("#pesoSca").val("120");
-  var selec = document.getElementById("pesoAni");
-  selec.focus();
-  selec.setSelectionRange(0, selec.value.length);
+  $("#pesoSca").val("140");
+  if (!isMobile()) {
+    var selec = document.getElementById("pesoAni");
+    selec.focus();
+    selec.setSelectionRange(0, selec.value.length);
+  }
+  return false;
 }
 
 function cTotal() {
   var anim = $('input[name=animales]:checked').val();
-  var sca = parseFloat($('input[name=scalas]:checked').val()) / 10;
+  var sca = round(parseFloat($('input[name=scalas]:checked').val()) / 10, 2);
   var pSca = $("#pesoSca").val();
   var pAni = $("#pesoAni").val();
   var aPrec = $("#aniPrec").val();
-  var tot = pAni * (aPrec - ((pAni - pSca) * sca));
-  var arr = [anim, sca, pSca, pAni, aPrec, tot];
+  if (isNaN(pSca) || isNaN(pAni) || isNaN(aPrec)) {
+    return false;
+  }
+  var nuevoP = aPrec - ((pAni - pSca) * sca)
+  var tot = pAni * nuevoP;
+  var arr = [anim, sca, pSca, pAni, aPrec, nuevoP, tot];
 
   var tableRef = document.getElementById("cvgHist").getElementsByTagName("tbody")[0];
   var newRow   = tableRef.insertRow(tableRef.rows.length);
-  for(i = 0; i < 6; i++) {
-    var newCell  = newRow.insertCell(i);
+  var newCell;
+  for(i = 0; i < 7; i++) {
+    newCell  = newRow.insertCell(i);
     var newText  = document.createTextNode(arr[i]);
     newCell.appendChild(newText);
   }
+  newCell.classList.add("ttl");
 
-  var selec = document.getElementById("pesoAni");
-  selec.focus();
-  selec.setSelectionRange(0, selec.value.length);
-  console.log(tot);
+  var sum = 0;
+  // iterate through each td based on class and add the values
+
+  $(".ttl").each(function() {
+
+      var value = $(this).text();
+      // add only if the value is number
+      if(!isNaN(value) && value.length != 0) {
+          sum += parseFloat(value);
+      }
+  });
+
+  $("#cmtot").html(sum);
+
+  if (!isMobile()) {
+    var selec = document.getElementById("pesoAni");
+    selec.focus();
+    selec.setSelectionRange(0, selec.value.length);
+  }
+
   return false;
 }
 
